eb0454a926422f3a2b8d6fd23a033aeffce530fa
zubbyonfire <zackbloundele@outlook.com>
2019-02-22 22:21:59 +0000

    UI Unit Tests and Logic implemented
    Began creating Unit Tests for the logic behind the UI Textbox
    Tests:
    - Removing white space from a string
    - Converting string to all Uppercase
    - Getting coordinates from a PLACE1,2,N
    
    Have then implemented the logic in the UIController script upon passing the test.
    
    To Do:
    - Continue test/implement logic of UIController: this includes making sure coordinates are valid, rasing event based on what a string contains.
    - Have the textbox linked up, so UIController will run when enter or button is pressed
    - Have strings added to the command box

---
+++ RobotCodeTest/Assets/Scripts/InputController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//What this script does:
+//Handles the logic behind reading the inputs from the input field
+//Either wait for button press or enter button pressed
+//Then take the inputted string, filter it accordingly - remove white space
+//If it's the Place command, get the Grid X,Y and direction facing
+//Finally dispatch Event based on the string
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Take in a string, filter it to remove white space - check to see if string contains 'X' phrase
+    /// </summary>
+    /// <param name="textInput"></param>
+    void FilterString(string textInput)
+    {
+        //Remove all white spaces in the string
+        //Check to see if string contains x,y,z
+        //If it's the place command - look for the brackets and commas
+    }
+
+    /// <summary>
+    /// Return the passed string with no empty spaces
+    /// </summary>
+    /// <param name="textInput"></param>
+    /// <returns></returns>
+    string RemoveWhiteSpace(string textInput)
+    {
+        textInput = textInput.Replace(" ", string.Empty);
+
+        return textInput;
+    }
+
+    /// <summary>
+    /// Return the passed string converted to uppercase
+    /// Though the text field forces to uppercase - so a just incase
+    /// </summary>
+    /// <param name="textInput"></param>
+    /// <returns></returns>
+    string ConvertToUpper(string textInput)
+    {
+        textInput = textInput.ToUpper();
+
+        return textInput;
+    }
+
+    /// <summary>
+    /// Return the coordinates in a PLACE command
+    /// Go to position 6 in the string, starting from their loop until first comma is found, adding each char to the test string
+    /// Once the comma has been found, add that to the Vector2.x (converting to a int)
+    /// Repeat for the Vector2.y - using a counter to make sure right Vector2 value is modified
+    /// If any values of the Vector2 are -1, return -1,-1, as it dosen't have a value coordinate position
+    /// </summary>
+    /// <param name="textInput"></param>
+    /// <returns></returns>
+    Vector2 ObtainCoordinates(string textInput)
+    {
+        //Coordinates to pass back
+        Vector2 coordinates = new Vector2(-1,-1);
+        //Stores the integer 
+        string strCoord = "";
+        //How many commas have we found (only looking for one)
+        int commaCounter = 0;
+
+        //Start at position 6 - thats when the number will start
+        for (int strPos = 6; strPos < textInput.Length; strPos++)
+        {
+            //If we don't find a comma, then add the char to the strCoord
+            if (textInput[strPos] != ',')
+            {
+                strCoord += textInput[strPos];
+            }
+            else if (commaCounter == 0) //Is this the first comma we found
+            {
+                //Try and int.parse the strCoord - if we can, set the coord.x to the coordPos
+                if (int.TryParse(strCoord, out int coordPos))
+                {
+                    coordinates.x = coordPos;
+                }
+                else
+                {
+                    //Else set to -1 - not a valid conversion
+                    coordinates.x = -1;
+                }
+
+                //Reset the strCoord to empty
+                strCoord = "";
+
+                //Increment comma counter - looking for 2nd one now
+                commaCounter++;
+            }
+            else if (commaCounter == 1)
+            {
+                if (int.TryParse(textInput, out int coordPos))
+                {
+                    coordinates.y = coordPos;
+                }
+                else
+                {
+                    coordinates.y = -1;
+                }
+            }
+        }
+
+        //If either x or y value is -1, then the coordinates are not valid return -1,-1
+        if (coordinates.x == -1 || coordinates.y == -1)
+        {
+            coordinates = new Vector2 (-1, -1);
+        }
+
+        //Return the coordinates value
+        return coordinates;
+    }
+}
---
+++ RobotCodeTest/Assets/Scripts/Tests/InputControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class InputControllerTests
+    {
+        [Test]
+        public void StringWhiteSpaceRemoved()
+        {
+            string stringCommand = " M Ove ";
+
+            stringCommand = stringCommand.Replace(" ", string.Empty);
+
+            Assert.IsFalse(stringCommand.Contains(" "), stringCommand);
+        }
+
+        [Test]
+        public void StringConvertedToUpper()
+        {
+            string stringCommand = "move";
+
+            stringCommand = stringCommand.ToUpper();
+
+            Assert.AreEqual(stringCommand, "MOVE");
+        }
+
+        [Test]
+        public void GetCoordinatesFromString()
+        {
+            //PLACE1,1,N
+            //Go to position 6 in the string, keep looping until we reach a comma
+            //Take that string and convert to a int
+            //Continue until reach next comma, convert to a int and add to vector
+            //Make sure to check if string is a valid conversion to int
+            //This method will work for any size of grid
+
+            string stringCommand = "PLACE 1,1,N";
+
+            string testString = "";
+
+            Vector2 coordinatePos = new Vector2(0,0);
+
+            int commaCounter = 0;
+
+            for (int i = 6; i < stringCommand.Length; i++)
+            {
+                if (stringCommand[i] != ',')
+                {
+                    testString += stringCommand[i];
+                } 
+                else if (commaCounter == 0)
+                {
+                    if (int.TryParse(testString, out int coordPos))
+                    {
+                        coordinatePos.x = coordPos;
+                    }
+                    else
+                    {
+                        coordinatePos.x = -1;
+                    }
+
+                    testString = "";
+
+                    commaCounter++;
+                } 
+                else if (commaCounter == 1)
+                {
+                    if (int.TryParse(testString, out int coordPos))
+                    {
+                        coordinatePos.y = coordPos;
+                    }
+                    else
+                    {
+                        coordinatePos.y = -1;
+                    }
+                }
+            }
+
+            Assert.AreEqual(coordinatePos, new Vector2(1, 1));
+        }
+    }
+}
